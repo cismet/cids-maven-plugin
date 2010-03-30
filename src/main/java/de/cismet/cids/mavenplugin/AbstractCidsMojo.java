47383e6ced7da51dce910a3526f6ac2f6749ebcf
Martin Scholl <martin.scholl@cismet.de>
2010-03-30 09:47:46 +0000

    [issue:1435] initial release

---
+++ src/main/java/de/cismet/cids/mavenplugin/AbstractCidsMojo.java
@@ -0,0 +1,35 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.mavenplugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * General Mojo for cids related maven plugin stuff
+ *
+ * @author   martin.scholl@cismet.de
+ * @version  $Revision$, $Date$
+ */
+public abstract class AbstractCidsMojo extends AbstractMojo {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String PROP_CIDS_CLASSPATH = "de.cismet.cids.classpath"; // NOI18N
+
+    //~ Instance fields --------------------------------------------------------
+
+    /**
+     * The enclosing maven project.
+     *
+     * @parameter  expression="${project}"
+     * @required   true
+     * @readonly   true
+     */
+    protected MavenProject projectmy;
+}
